7de7aaa6eea11a6104a40c88569df58cf92dda34
XRJ4881 <javpraaco@alum.us.es>
2026-05-06 12:08:59 +0200

    correccion ruta integraciones

---
+++ src/front/src/routes/integrations/+page.svelte
@@ -0,0 +1,5 @@
+<ul>
+	<li><a href="./integrations/integrations_javi">Integrations javi</a></li>
+	<li><a href="./integrations/integrations_ignacio">Integrations Ignacio</a></li>
+	<li><a href="./integrations/integrations_adrian">Integrations Adrian</a></li>
+</ul>
